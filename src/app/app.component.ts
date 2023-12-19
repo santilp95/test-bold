f9b46b7ca02fa3a57493fce72df53700623b0b63
Santiago DAV <santiago.lopez@davivienda.com>
2023-12-19 12:42:43 -0500

    style: :lipstick: margin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './atoms/header/header.component';
 import { SalesSummaryCardComponent } from './molecules/sales-sumary-card/sales-summary-card.component';
+import { ButtonGroupComponent } from './molecules/button-group/button-group.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, SalesSummaryCardComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SalesSummaryCardComponent, ButtonGroupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
