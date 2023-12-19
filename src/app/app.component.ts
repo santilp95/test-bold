719d626984876baf3dd8811a16302b4b870ed22d
Santiago DAV <santiago.lopez@davivienda.com>
2023-12-18 21:17:16 -0500

    style: :lipstick: app-header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './atoms/header/header.component';
 
 @Component({
   selector: 'app-root',
