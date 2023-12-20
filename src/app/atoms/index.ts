a40f2d5f2ff513d7ab7cf33d3d8876b6ba24766b
Santiago DAV <santiago.lopez@davivienda.com>
2023-12-20 00:22:05 -0500

    feat: :zap:  submit-component

--- src/app/atoms/index.ts
+++ src/app/atoms/index.ts
@@ -1,3 +1,4 @@
+export  { CustomCheckboxComponent } from "./custom-checkbox/custom-checkbox.component";
 export  { HeaderComponent } from "./header/header.component";
 export  { SelectableButtonComponent } from "./selectable-button/selectable-button.component";
-export  { CustomCheckboxComponent } from "./custom-checkbox/custom-checkbox.component";
+export { SubmitButtonComponent } from "./submit-button/submit-button.component";
