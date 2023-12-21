f03162d8d5cbb42976389ff56ff48b1499049eaf
Santiago DAV <santiago.lopez@davivienda.com>
2023-12-20 21:22:15 -0500

    feat: :lipstick: custom-table

---
+++ src/app/molecules/custom-table/custom-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-table.component.html',
+  styleUrl: './custom-table.component.css'
+})
+export class CustomTableComponent {
+
+}
