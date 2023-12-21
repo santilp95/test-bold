e99babe7bf319194b536c52fe4380cad6d553b2d
Santiago DAV <santiago.lopez@davivienda.com>
2023-12-20 21:23:50 -0500

    feat: :lipstick: custom-table

--- src/app/molecules/index.ts
+++ src/app/molecules/index.ts
@@ -1,3 +1,4 @@
 export { ButtonGroupComponent } from "./button-group/button-group.component";
+export { CustomTableComponent } from "./custom-table/custom-table.component";
 export { FilterToggleComponent } from "./filter-toggle/filter-toggle.component";
 export { SalesSummaryCardComponent } from "./sales-sumary-card/sales-summary-card.component";
